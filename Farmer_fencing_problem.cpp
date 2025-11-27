6883a0f9bfc06df8ff6824f78e05f07f84a37a4f
geekscse08-cmyk <geeks.cse08@gmail.com>
2025-11-27 17:26:10 +0530

    Add files via upload

---
+++ Farmer_fencing_problem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int l, b, area, perimeter ; 
+	cout << "Enter length : " ;
+	cin >> l ;
+	cout << "Enter width : " ;
+	cin >> b ;
+	
+	area=l*b;
+	perimeter=2*(l+b);
+	
+	cout << "Area = " << area << endl;
+	cout << "Perimeter = " << perimeter << endl;
+	return 0;
+}
