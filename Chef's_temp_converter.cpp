4c6388898b985ff39e0458f65f1da9604611b696
geekscse08-cmyk <geeks.cse08@gmail.com>
2025-11-27 17:34:08 +0530

    Add files via upload

---
+++ Chef's_temp_converter.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+	float C , F ;
+	cout << "Enter the temperature in celsius : " ;
+	cin >> C ;
+	
+    	F= (C*9/5)+32 ;
+	 cout<< "Temperature in Farhenheit = " << F << endl;
+	
+	
+		return 0;
+
+}
